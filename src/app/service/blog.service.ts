9932f0d44a230c1b299212348074521ab98c91bd
dankbilbo <buitrungnghia1.11@gmail.com>
2021-03-23 16:35:56 +0700

    get blog

---
+++ src/app/service/blog.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {BlogIn} from "../model/in/BlogIn";
+import {environment} from "../../environments/environment";
+import {Blog} from "../model/Blog";
+import {SystemResponse} from "../model/SystemResponse";
+
+const API_URL = `${environment.apiURL}`
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class BlogService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  addBLog(blog: Blog): Observable<BlogIn> {
+    return this.httpClient.post<BlogIn>(`${API_URL}/blogs`, blog);
+  }
+
+  // updateBlog(blog : )
+
+  getBlog(id : number) : Observable<SystemResponse>{
+    console.log(id);
+    return this.httpClient.get<SystemResponse>(`${API_URL}/blogs/${id}`);
+  }
+}
