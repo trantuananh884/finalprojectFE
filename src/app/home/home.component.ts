71df4610e54323dc173a825c4220e4693d326c58
Tran Tuan Anh <trantuananh884@gmail.com>
2021-03-25 13:20:14 +0700

    finish home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BlogService} from '../service/blog.service';
 import {Blog} from '../model/in/Blog';
+import {PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,17 @@ import {Blog} from '../model/in/Blog';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-blogs? :Blog[];
+blogs? :Blog[]=[];
 blogtop5viewd? :Blog[];
 blogtop5liked?:Blog[];
+pageSlice;
   constructor(private blogService : BlogService) { }
 
   ngOnInit(): void {
     this.blogService.getAllPublicBlogs().subscribe(res=>{
       console.log(res)
       this.blogs= res.data;
+      this.pageSlice= this.blogs.slice(0,3)
     })
     this.blogService.get5topviewed().subscribe(res=>{
       this.blogtop5viewd=res.data;
@@ -24,4 +27,15 @@ blogtop5liked?:Blog[];
     })
   }
 
+  OnPageChange(envent: PageEvent) {
+  console.log("phan trang dược gọi")
+    const startIndex = envent.pageIndex*envent.pageSize;
+  console.log(envent.pageIndex)
+    let endIndex = startIndex +envent.pageSize;
+    if(endIndex>this.blogs.length){
+      endIndex =this.blogs.length;
+    }
+    this.pageSlice=this.blogs.slice(startIndex,endIndex);
+
+  }
 }
