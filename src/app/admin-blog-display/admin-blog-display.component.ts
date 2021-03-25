de3d2539385be8850f57cdb3a5ff787427d37668
Tran Tuan Anh <trantuananh884@gmail.com>
2021-03-25 19:08:00 +0700

    finishing deleting blog

--- src/app/admin-blog-display/admin-blog-display.component.ts
+++ src/app/admin-blog-display/admin-blog-display.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Blog} from '../model/in/Blog';
+import {BlogService} from '../service/blog.service';
+import {AdminBlogService} from '../service/admin/admin-blog.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin-blog-display',
@@ -9,10 +12,18 @@ import {Blog} from '../model/in/Blog';
 export class AdminBlogDisplayComponent implements OnInit {
 @Input() blog?:Blog
   style?: string;
-  constructor() { }
+  constructor(private blogAdminService: AdminBlogService,private router:Router) { }
 
   ngOnInit(): void {
     this.style = "background-image: url(" + this.blog.previewImageURl +")";
   }
 
+  delete(id: number) {
+    console.log(id)
+    this.blogAdminService.delete(id).subscribe(res=>{
+      console.log(res);
+      this.router.navigateByUrl['../usermng']
+    },error => (console.log(error)))
+
+  }
 }
